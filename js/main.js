c468ff3023c02d0185f0145040372a407605ef5e
l-bissell78429 <43183061+l-bissell78429@users.noreply.github.com>
2019-03-12 15:01:47 -0400

    started Javascript

--- js/main.js
+++ js/main.js
@@ -7,6 +7,33 @@
 //modern JS
 //alert(`Hello ${firstName}`)
 
-//console.log("Hello" + firstName);
-
 console.log("javascript linked up");
+
+
+	let musicicons = document.querySelector('#musicIcons');
+	let plain = document.querySelector('.plainSock'),
+		dapper = document.querySelector('.dapper'),
+		drum = document.querySelector('.drum'),
+		giraffe = document.querySelector('.giraffe',)
+		hair = document.querySelector('.hair');
+	let dropzones = document.querySelector('.drop-zone');
+
+
+
+
+
+	function initDrag() {
+		musicicons.querySelectorAll('img').forEach(img => {
+			// queryselectorall inspect img
+			img.addEventListener("dragstart", function(e) {
+				console.log('dragstart')
+   		
+	function removePlayingClass(event) {
+			
+	}
+
+				e.dataTransfer.setData("text/plain", this.id);
+				
+	    });
+	});
+}

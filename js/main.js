fc1be6dd8226a88dc24c41dae68ef2bc26bed422
l-bissell78429 <43183061+l-bissell78429@users.noreply.github.com>
2019-03-27 18:32:20 -0400

    audio needs fix

--- js/main.js
+++ js/main.js
@@ -1,56 +1,117 @@
-console.log("javascript linked up");
+(() => {
+	console.log("javascript linked up");
 
-	const icons = ["one", "two", "three", "four"];
+	// set up variables first
+	let musicicons  = document.querySelectorAll('#musicIcons img');
 
-	let musicicons = document.querySelectorAll('#musicIcons img');
-	let plain      = document.querySelector('.plainSock'),
-		dapper     = document.querySelector('.dapper'),
-		drum       = document.querySelector('.drum'),
-		giraffe    = document.querySelector('.giraffe'),
-		hair       = document.querySelector('.hair');
-	let dropzones  = document.querySelector('.drop-zone');
+	let dapAudio = new Audio('music/beat.mp3'),
+	    drumAudio = new Audio('music/drums.mp3'),
+	    girAudio = new Audio('music/piano.mp3'),
+ 	    hairAudio = new Audio('music/robot-voice.mp3');
+
+	let dropDapper  = document.querySelector('.dzDapper'),
+		dropDrum    = document.querySelector('.dzDrum'),
+		dropGiraffe = document.querySelector('.dzGiraffe'),
+		dropHair    = document.querySelector('.dzHair');
+
+	let dapper      = document.querySelector('.dapper'),
+		drum        = document.querySelector('.drum'),
+		giraffe     = document.querySelector('.giraffe',)
+		hair        = document.querySelector('.hair');
+
+	let fdapper     = document.getElementById('one'),
+	    fdrum       = document.getElementById('two'),
+	    fgiraffe    = document.getElementById('three'),
+	    fhair       = document.getElementById('four');
+
+	let dropzones   = document.querySelectorAll('.drop-zone');
 
 
 
-	function initDrag() {
-		musicicons.querySelectorAll('img').forEach(img => {
-			// queryselectorall inspect img
-			img.addEventListener("dragstart", function(e) {
-				console.log('dragstart')
-   		
-	function removePlayingClass(event) {
-			
-	}
+	function playDapper(){
+		dapperAudio.currentTime = 0;
+		dapperAudio.loop = true;
+		dapperAudio.play();
+	};
 
-				e.dataTransfer.setData("text/plain", this.id);
-				
-	    });
+	function playDrum(){
+		dapperAudio.currentTime = 0;
+		dapperAudio.loop = true;
+		drumAudio.play();
+	};
+
+	function playGiraffe(){
+		dapperAudio.currentTime = 0;
+		dapperAudio.loop = true;
+		giraffeAudio.play();
+	};
+
+	function playHair(){
+		dapperAudio.currentTime = 0;
+		dapperAudio.loop = true;
+		hairAudio.play();
+	};
+
+
+
+
+
+
+
+	function drag() {
+	dropzones.querySelectorAll('img').forEach(img =>{
+		img.addEventListener('dragstart', function(e){
+			console.log('dragging...');
+			e.dataTransfer.setData('text/plain', this.id);
+		});
 	});
 }
 
+	dropzones.forEach(dropzone => {
+	dropzone.addEventListener('dragover', function(e){
+		console.log('dragging over...')
+		e.preventDefault();
+})
+});
 
 
 
 
-	dropzones.forEach(zone => {
-		zone.addEventListener("dragover", function(e) {
-			e.preventDefault();
-             console.log('dragover')
-		});
 
-		zone.addEventListener("drop", function(e) {
-			e.preventDefault();
-             console.log('dragover')
-			let iconDrop = e.target;
-				while (iconDrop !== 0 && ! iconDrop.classList.contains("drop-zone")) {
-				iconDrop = iconDrop.parentNode;
-			}
 
-			if (iconDrop && iconDrop.childNodes.length > 0) {
-				return false;
-				e.preventDefault();
-			}
-			let icon = e.dataTransfer.getData("text/plain");
-			e.target.appendChild(document.querySelector(`#${piece}`));
-		});
-	});
+	one.addEventListener('drop', function(e){
+	e.preventDefault();
+	console.log('dropped dapper');
+		dropDapper.classList.remove('invisible');
+		playDapper();
+});
+
+
+	two.addEventListener('drop', function(e){
+	e.preventDefault();
+	console.log('dropped drum');
+		dropDrum.classList.remove('invisible');
+		playDrum();
+});
+
+
+	three.addEventListener('drop', function(e){
+	e.preventDefault();
+	console.log('dropped giraffe');
+		dropGiraffe.classList.remove('invisible');
+		playGiraffe();
+});
+
+
+	four.addEventListener('drop', function(e){
+	e.preventDefault();
+	console.log('dropped hair');
+		dropHair.classList.remove('invisible');
+		playHair();
+});
+
+
+
+
+
+})(); 
